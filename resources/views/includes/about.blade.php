4572f28c8363b194920aa6d1160f62d30fd763fb
陳泓安 <alpschen05@gmail.com>
2018-08-15 18:05:00 +0800

    1.add read more

--- resources/views/includes/about.blade.php
+++ resources/views/includes/about.blade.php
@@ -18,7 +18,7 @@
                     <h3>My friends & I
                         <span class="subtitle">Comradeship.</span>
                     </h3>
-                    <p>Along the way on my educational path, I met several indescribable soul mates. Especially after high school,
+                    <p class="desc">Along the way on my educational path, I met several indescribable soul mates. Especially after high school,
                         owing to our school having merely “males”, I encountered more friends similar to me, possessing hobbies
                         in common. Thanks to the environment, I could express myself as freely as necessary. I could wear
                         shirtless and slippers all the time, declare anything I want, no need to care about the appearance
@@ -34,13 +34,13 @@
                 <div class="fbox-media">
                     <img src="{{asset('images/home/about/math_application.png')}}" alt="ooooops">
                 </div>
-                <div class="fbox-desc portfolio-desc">
-                    <a href="#" data-href="#section-work">
-                        <h3>My mathematics application
-                            <span class="subtitle">會考會考的數學。</span>
-                        </h3>
-                    </a>
-                    <p>In 2018, I commenced the self-studying of React Native. Utilizing this language, I constructed this math-problem
+                <div class="fbox-desc">
+                    <!-- <a href="#" data-href="#section-work"></a> -->
+                    <h3>My mathematics application
+                        <span class="subtitle">會考會考的數學。</span>
+                    </h3>
+                    <!-- </a> -->
+                    <p class="desc">In 2018, I commenced the self-studying of React Native. Utilizing this language, I constructed this math-problem
                         application for the junior high school students in Taiwan. The title “會考會考的數學” consists of a hilarious
                         playing on the Chinese characters. “會考” stands for the high school entering test in Taiwan, while
                         simultaneously meaning “will be tested” in common speaking. In addition, “數學” is the word for “mathematics”.
@@ -59,7 +59,7 @@
                     <h3>My summer camp
                         <span class="subtitle">2017 Microfims & Informations camp.</span>
                     </h3>
-                    <p>The summer camp took place in Jieshou Junior High School, an aboriginal school in a township in Taiwan.
+                    <p class="desc">The summer camp took place in Jieshou Junior High School, an aboriginal school in a township in Taiwan.
                         I organized the camp with my friends in my high school. Our target was to spread the seed of Informatics
                         and Microfilm into rural places due to their lack of comprehension on this field, which will be the
                         prevailing domain in the future (STEM).
@@ -79,7 +79,7 @@
                     <h3>My school
                         <span class="subtitle">Taipei Municipal Jianguo High School</span>
                     </h3>
-                    <p>Being the best high school in Taiwan, we endure pressure but are always proud of our school. We are regarded
+                    <p class="desc">Being the best high school in Taiwan, we endure pressure but are always proud of our school. We are regarded
                         as the “camel passenger” owing to the shriveled grass surrounded by our running track. I enchant
                         my school, not only because of the free atmosphere in the campus, but also possessing the most invaluable
                         assets -- extraordinary friends. After three years of the miraculous experiences, I’ve comprehended
@@ -97,7 +97,7 @@
                     <h3>My Family
                         <span class="subtitle">Incredible and Marvelous</span>
                     </h3>
-                    <p>This photo was taken during the feast in Chinese New Year. My parents were sitting in the middle beside
+                    <p class="desc">This photo was taken during the feast in Chinese New Year. My parents were sitting in the middle beside
                         me. On the left side were my grandparents. My grandma is a five-star chef. She always prepare several
                         creative but traditional meals when we visit. In the bottom were my uncle’s family. His wife is an
                         American, and when we met up, I liked to chat with his sons, who always shared with me Airsoft and
@@ -122,7 +122,7 @@
                     <h3>My Future
                         <span class="subtitle">Release the Flying Lanterns</span>
                     </h3>
-                    <p>“Flying Lanterns” are an exclusive culture in Taiwan. People write their dreams on the lanterns, each
+                    <p class="desc">“Flying Lanterns” are an exclusive culture in Taiwan. People write their dreams on the lanterns, each
                         color representing distinct connotations. For instance, purple regards success on learning and educations.
                         The activity takes place in “Pinshi”, a town located in the north of Taipei. You could see various
                         languages written on innumerous lanterns. Ultimately, you release the lanterns like a hot air balloon,
